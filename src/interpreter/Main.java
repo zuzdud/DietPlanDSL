b6c6d262400653b27ae64dbba05dabeab5c326b5
Karolina <272888@student.pwr.edu.pl>
2026-03-29 20:40:33 +0200

    Added matching allergens and allergies from input objects. Null attributes don't work

--- src/interpreter/Main.java
+++ src/interpreter/Main.java
@@ -2,10 +2,14 @@ package interpreter;
 
 import grammar.firstLexer;
 import grammar.firstParser;
+import models.Dish;
+import models.Guest;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -19,5 +23,29 @@ public class Main {
         ParseTree tree = parser.program();
         System.out.println(tree.toStringTree(parser));
 
+        // create dish visitor
+        DishVisitor visitor = new DishVisitor();
+        visitor.visit(tree);
+
+        // importing lists of guest and dish obj
+        List<Guest> guests = visitor.guests;
+        List<Dish> dishes = visitor.dishes;
+
+        // checking for allergies in guests and matching the dishes for them
+        for (Guest guest : guests) {
+            System.out.println("\nDish for: " + guest.name);
+            for (Dish dish : dishes) {
+                boolean safe = true;
+                for (String allergy : guest.allergies) {
+                    if (dish.allergens.contains(allergy)) {
+                        safe = false;
+                        break;
+                    }
+                }
+                if (safe) {
+                    System.out.println("- " + dish.name);
+                }
+            }
+        }
     }
 }
---
+++ src/models/Dish.java
@@ -0,0 +1,9 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dish {
+    public String name;
+    public List<String> allergens = new ArrayList<>();
+}
